d49ccbff03d49e5fa72fefedf3fc6ee7d69c016a
Emil <emil.andrzejewski@gmail.com>
2020-08-15 00:00:48 +0200

    login & logout

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Component, OnDestroy } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import * as firebase from "firebase";
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent implements OnInit {
+export class BsNavbarComponent implements OnDestroy {
+  isLogged: boolean = false;
+  user: firebase.User;
+  username: string;
+  subs: Subscription[];
+  sub: Subscription;
 
-  constructor() { }
+  constructor(
+    private afAuth: AngularFireAuth,
+    private router: Router  
+  ) { 
+    this.sub = this.afAuth.authState.subscribe(user => 
+    {
+      this.user = user;
+      if(user) {
+        this.router.navigate(['/']);
+      }
+    });
+  }
 
-  ngOnInit(): void {
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
+
+  logout() {
+    this.afAuth.signOut();
+    this.isLogged = false;
+    this.router.navigate(['/login']);
   }
 
 }
